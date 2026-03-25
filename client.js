864e6d83015c769e7798aef59f3b50ea13217aac
Lam Nguyen <lam.nguyen@pascalsoftware.com>
2026-03-25 15:17:40 +0700

    feat(websocket): make request upgrade from client and server receive (no implementation yet)

--- client.js
+++ client.js
@@ -1,16 +1,17 @@
-const WebSocket = require("ws");
+const WebSocket = require("./lib/websocket");
+// const WebSocket = require("ws");
 
 const ws = new WebSocket("ws://localhost:8080");
 
-ws.on("open", () => {
-  console.log("Connected");
-  // ws.send("Hello, server! - from Client");
-});
+// ws.on("open", () => {
+//   console.log("Connected");
+// ws.send("Hello, server! - from Client");
+// });
 
-ws.on("message", (data) => {
-  console.log("Received: %s", data);
-});
+// ws.on("message", (data) => {
+//   console.log("Received: %s", data);
+// });
 
-ws.on("close", () => {
-  console.log("Disconnected");
-});
+// ws.on("close", () => {
+//   console.log("Disconnected");
+// });
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const { WebSocketServer } = require("ws");
-// const WebSocketServer = require("./lib/websocket-server");
+// const { WebSocketServer } = require("ws");
+const WebSocketServer = require("./lib/websocket-server");
 
-const wss = new WebSocketServer({
-  port: 8080,
-});
+const PORT = 8080;
+const wss = new WebSocketServer(
+  {
+    port: PORT,
+  },
+  () => {
+    console.log("Server is listening on %s", PORT);
+  },
+);
 
 wss.on("connection", (ws) => {
   console.log("Connection established");
